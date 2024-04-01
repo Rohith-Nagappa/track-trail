54ab6697e5c7d474a9b98e0fc61a12e747f34c08
Rohith-Nagappa <rohithankith2017@gmail.com>
2024-04-01 20:39:08 +0530

    ss

--- Frontend/src/Components/Form/Form.js
+++ Frontend/src/Components/Form/Form.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css"
 
 export default function Form() {
    const [inputState,setInputState]= useState({
@@ -12,6 +14,10 @@ export default function Form() {
 
     const {title,amount,date,category,description} = inputState;
 
+    const handleInput = name => e => {
+       setInputState({...inputState,[name]:e.target.value})
+    }
+
   return (
     <Formstyled>
       <div className='input-control'>
@@ -22,6 +28,26 @@ export default function Form() {
         placeholder='Income Title'
         onChange={handleInput('title')}
         />
+      </div>
+      <div className='input-control'>
+        <input 
+        type='text'
+        value={amount}
+        name={'amount'}
+        placeholder='Income amount'
+        onChange={handleInput('amount')}
+        />
+      </div>
+      <div className='input-control'>
+      <DatePicker 
+      id='date'
+      placeholderText='Enter A Date'
+      selected={date}
+      dateFormat='dd/mm/yyyy'
+      onChange={()=>{
+        setInputState({...inputState,date:date})
+      }}
+      />
       </div>
     </Formstyled>
   )
