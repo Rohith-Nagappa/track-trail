a11b8ef47d803a6a033efaaf3964f42c8c985567
Rohith-Nagappa <rohithankith2017@gmail.com>
2024-04-01 20:14:15 +0530

    d

--- Frontend/src/Components/Form/Form.js
+++ Frontend/src/Components/Form/Form.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 export default function Form() {
+   const [inputState,setInputState]= useState({
+        title:'',
+        amount: '',
+        date: '',
+        category: '',
+        description: '',
+    })
+
+    const {title,amount,date,category,description} = inputState;
+
   return (
     <Formstyled>
-      Form
+      <div className='input-control'>
+        <input 
+        type='text'
+        value={title}
+        name={'title'}
+        placeholder='Income Title'
+        onChange={handleInput('title')}
+        />
+      </div>
     </Formstyled>
   )
 }
