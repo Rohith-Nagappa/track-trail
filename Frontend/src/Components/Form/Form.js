1578967d612e2f6832373e47de0f30e06f52a433
Rohith-Nagappa <rohithankith2017@gmail.com>
2024-04-02 19:25:18 +0530

    mm

--- Frontend/src/Components/Form/Form.js
+++ Frontend/src/Components/Form/Form.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import styled from 'styled-components';
 import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css"
+import "react-datepicker/dist/react-datepicker.css";
+import { useGlobalContext } from '../../context/globalContext';
 
-export default function Form() {
+
+const Form = () => {
+    const {addIncome}= useGlobalContext();
    const [inputState,setInputState]= useState({
         title:'',
         amount: '',
@@ -18,8 +21,13 @@ export default function Form() {
        setInputState({...inputState,[name]:e.target.value})
     }
 
+    const handleSubmit = e => {
+        e.preventDefault()
+        addIncome(inputState)   //we are sending payload as inputstate to addIncome
+    }
+
   return (
-    <Formstyled>
+    <Formstyled onSubmit={handleSubmit}>
       <div className='input-control'>
         <input 
         type='text'
@@ -49,6 +57,22 @@ export default function Form() {
       }}
       />
       </div>
+      <div className='selects input-control'>
+        <select required value={category} name='category' id='category' onChange={handleInput('category')}> 
+             <option value="" disabled>Select Option</option>
+             <option value="salary" disabled>Salary</option>
+             <option value="freelancing" disabled>Freelancing</option>
+             <option value="investments" disabled>Investments</option>
+             <option value="stocks" disabled>Stocks</option>
+             <option value="bitcoin" disabled>Bitcoin</option>
+             <option value="bank" disabled>Bank Transfer</option>
+             <option value="youtube" disabled>Youtube</option>
+             <option value="other" disabled>Other</option>
+        </select>
+      </div>
+      <div className='submit-btn'>
+        <button>Add Income</button>
+      </div>
     </Formstyled>
   )
 }
@@ -56,3 +80,5 @@ export default function Form() {
 const Formstyled = styled.form`
 
 `;
+
+export default Form;
--- Frontend/src/Components/Income/Income.js
+++ Frontend/src/Components/Income/Income.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { InnerLayout } from "../../styles/Layouts";
 import { useGlobalContext } from "../../context/globalContext";
+import Form from "../Form/Form";
 
 const Income = () =>{
     const {addIncome}= useGlobalContext()
@@ -10,7 +11,9 @@ const Income = () =>{
             <InnerLayout>
                 <h1>Income</h1>
                 <div className="income-content">
-                    <div className="form-container"></div>
+                    <div className="form-container">
+                        <Form/>
+                    </div>
                     <div className="incomes">
 
                     </div>
