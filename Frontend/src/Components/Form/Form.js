5fad8a2ef46af75f29eee78481127ef51eff8f20
Rohith-Nagappa <rohithankith2017@gmail.com>
2024-04-05 19:35:37 +0530

    aa

--- Frontend/src/Components/Form/Form.js
+++ Frontend/src/Components/Form/Form.js
@@ -1,100 +1,10 @@
-// import React, { useState } from 'react'
-// import styled from 'styled-components';
-// import DatePicker from 'react-datepicker';
-// import "react-datepicker/dist/react-datepicker.css";
-// import { useGlobalContext } from '../../context/globalContext';
-
-
-// const Form = () => {
-//     const {addIncome}= useGlobalContext();
-//    const [inputState,setInputState]= useState({
-//         title:'',
-//         amount: '',
-//         date: null,
-//         category: '',
-//         description: '',
-//     })
-
-//     const {title,amount,date,category,description} = inputState;
-
-//     // const handleInput = name => e => {
-//     //    setInputState({...inputState,[name]:e.target.value})
-//     // }
-
-//     const handleInput = (name, e) => {
-//         setInputState({ ...inputState, [name]: e.target.value });
-//     };
-    
-
-//     const handleDateChange = date => {
-//         setInputState({ ...inputState, date }); // Update date in state
-//     };
-
-//     const handleSubmit = e => {
-//         e.preventDefault()
-//         addIncome(inputState)   //we are sending payload as inputstate to addIncome
-//     }
-
-//   return (
-//     <Formstyled onSubmit={handleSubmit}>
-//       <div className='input-control'>
-//         <input 
-//         type='text'
-//         value={title}
-//         name={'title'}
-//         placeholder='Income Title'
-//         onChange={handleInput('title')}
-//         />
-//       </div>
-//       <div className='input-control'>
-//         <input 
-//         type='text'
-//         value={amount}
-//         name={'amount'}
-//         placeholder='Income amount'
-//         onChange={handleInput('amount')}
-//         />
-//       </div>
-//       <div className='input-control'>
-//       <DatePicker 
-//       id='date'
-//       placeholderText='Enter A Date'
-//       selected={date}
-//       dateFormat='dd/MM/yyyy' // Correct date format
-//       onChange={handleDateChange} // Update date onChange
-//       />
-//       </div>
-//       <div className='selects input-control'>
-//         <select required value={category} name='category' id='category' onChange={handleInput('category')}> 
-//              <option value="" >Select Option</option>
-//              <option value="salary" >Salary</option>
-//              <option value="freelancing" >Freelancing</option>
-//              <option value="investments" >Investments</option>
-//              <option value="stocks" >Stocks</option>
-//              <option value="bitcoin" >Bitcoin</option>
-//              <option value="bank" >Bank Transfer</option>
-//              <option value="youtube" >Youtube</option>
-//              <option value="other" >Other</option>
-//         </select>
-//       </div>
-//       <div className='submit-btn'>
-//         <button>Add Income</button>
-//       </div>
-//     </Formstyled>
-//   )
-// }
-
-// const Formstyled = styled.form`
-
-// `;
-
-// export default Form;
-
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { useGlobalContext } from '../../context/globalContext';
+import Button from '../Button/Button';
+import { plus } from '../../utils/icons';
 
 const Form = () => {
 
@@ -153,7 +63,7 @@ const Form = () => {
                 />
             </div>
             <div className='selects input-control'>
-                <select required value={category} name='category' id='category' onChange={(e) => handleInput('category', e)}> 
+                <select required value={category} name='category' id='category'  onChange={(e) => handleInput('category', e)}> 
                     <option value="" >Select Option</option>
                     <option value="salary" >Salary</option>
                     <option value="freelancing" >Freelancing</option>
@@ -173,7 +83,9 @@ const Form = () => {
                 </textarea>
             </div>
             <div className='submit-btn'>
-                <button>Add Income</button>
+                <Button name={'Add income'} icon={plus} bPad={'.8rem 1.6rem'} bRad={'30px'} bg={'black'}
+                color={'#fff'} 
+                />
             </div>
         </Formstyled>
     );
@@ -191,6 +103,45 @@ const Formstyled = styled.form`
           padding: .5rem 1rem;
           border-radius: 5px;
           border: 2px solid #fff;
+          background: transparent;
+          resize: none;
+          box-shadow: 0px 1px 15px rgba(0,0,0,0.06);
+          color: rgba(34,34,96,0.9);
+          &::placeholder{
+            color: rgba(34,34,96,0.4);
+          }
+      };
+
+      .input-control{
+        input{
+            width: 100%;
+        }
+        
+      }
+
+      .selects{
+        display: flex;
+        justify-content: flex-end;
+        select{
+            color: rgba(34,34,96,0.4);
+            &:focus, &:active{
+                color: rgba(34,34,34,1);
+            }
+
+        }
+      }
+
+      .submit-btn{
+        button{
+            box-shadow: 0px 1px 15px rgba(0,0,0,0.06);
+            background-color: pink;
+            padding: 10px;
+            border-radius: 10px;
+            &:hover{
+                background: var(--color-green) !important;
+                transition: all 0.4s ease-in-out;
+            }
+        }
       }
 
 `;
